1b8349cd8879ec030fd167abd8cc8cc9c23353e0
askjmyyyojqa <askjmyyyojqa@naver.com>
2023-12-16 14:32:52 +0900

    carTest

---
+++ java/carTest-hj/src/carTest.java
@@ -0,0 +1,13 @@
+public class carTest {
+    public static void main(String[] args) {
+        String model = "BMW528i";
+        String distance = "100000km";
+        int price = 90000000;
+        String company = "BMW";
+        char type = 'A';
+        boolean auto = true;
+        int year = 2010;
+        float gasmi = 12.5F;
+        System.out.println(gasmi);
+    }
+}
