09e686ed9b6c639dd0ba314b8098349a1493ea9a
askjmyyyojqa <askjmyyyojqa@naver.com>
2023-12-27 19:17:40 +0900

    findAPI

---
+++ java/VDATest-hj/src/VDATest.java
@@ -0,0 +1,26 @@
+public class VDATest {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        int b = a;
+        int c = b * 10;
+        int sum = 0;:q
+        sum += 1;
+        sum += 2;
+        sum += 3;
+        sum += 4;
+        sum += 5;
+
+        int x = 10;
+        int y = 20;
+        int temp = x;
+        x = y;
+        y = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("sum = " + sum);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
---
+++ java/findAPI-hj/src/findAPI.java
@@ -0,0 +1,13 @@
+public class findAPI {
+    public static void main(String[] args) {
+        int a; //변수 선언
+        a = 10;
+        String s;
+        java.lang.String ss;
+        s="APPLE";
+        ss="Orange";
+        System.out.println("s = " + s);
+        System.out.println("ss = " + ss);
+        System.out.println(s.length());
+    }
+}
